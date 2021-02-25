95d902493dac982dd4979d25d25e8ee3a72d2e44
benkwon <kwon5891@gmail.com>
2021-02-25 23:49:45 +0900

    @EnableAutoConfiguration 관련 import

--- src/contract-test/java/com/shoppingmall/cart/samplePact.java
+++ src/contract-test/java/com/shoppingmall/cart/samplePact.java
@@ -20,6 +20,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
