b03bd18519d8293e0ce55a5905d62fb7a098b566
herjn <leejungin199@naver.com>
2021-03-04 00:01:33 +0900

    [Update] client,전체코드수정

---
+++ Shopping-Cart/src/main/java/com/shopping/cart/cart/CartApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.cart.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
--- Shopping-Cart/src/main/java/com/shopping/cart/cart/cart/CartController.java
+++ Shopping-Cart/src/main/java/com/shopping/cart/cart/cart/CartController.java
@@ -34,7 +34,6 @@ public class CartController {
     @GetMapping("/{id}")
     public JSONObject retrieveAllPostsByUser(@PathVariable int id) throws ParseException {
         String output = "{" + "id:" + resturant.getIdtoid(id) + ", totalprice:" + resturant.getTotalpricetoid(id) + ", product_list:" + resturant.getProduct_listtoid(id) + "}";
-
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(output);
         JSONObject jsonObj = (JSONObject) obj;
@@ -44,9 +43,9 @@ public class CartController {
 
     @PostMapping("")
     public String addProduct(@RequestBody Param param){
-        Product product = restTemplate.getForObject("http://localhost:8091/".concat(Integer.toString(param.getProduct_id())), Product.class);
+        Product product = restTemplate.getForObject("http://localhost:8088/".concat(Integer.toString(param.getProduct_id())), Product.class);
         resturant.setProduct_listtoid(param.getUser_id(),param.getProduct_id(),param.getCount());
-        resturant.setTotalpricetoid(param.getUser_id(),(product.getPrice() * param.getCount()));
+        resturant.setTotalpricetoid(param.getUser_id(),(product.getPrice()*param.getCount()));
 
         return "Post Successful";
     }
