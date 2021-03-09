9c9ec776bafcc11e38e7173c23a8b3c6f7042d09
JeongEunJi <wjdwl270@naver.com>
2021-03-09 21:39:14 +0900

    [Delete]master code

--- src/main/java/com/shopping/shoppingmallcart/cart/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.shoppingmallcart.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-}
-
--- src/main/java/com/shopping/shoppingmallcart/cart/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopping.shoppingmallcart.cart;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
